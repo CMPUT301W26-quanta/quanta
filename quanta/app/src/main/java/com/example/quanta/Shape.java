4ec1dcfee254096252f2e99e83e2cfe363a10557
Komal Kaur <komalpr1@ualberta.ca>
2026-01-28 18:19:43 -0700

    Added color attribute
    Added color attribute to Shape class

--- quanta/app/src/main/java/com/example/quanta/Shape.java
+++ quanta/app/src/main/java/com/example/quanta/Shape.java
@@ -4,6 +4,8 @@ public abstract class Shape {
     private int x;
     private int y;
 
+    private String color = "blue";
+
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
