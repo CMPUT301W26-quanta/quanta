f091da13d609faf37ec2a5ac915cdfc0f0153993
GlitchlessCode <talyn.veugelers@gmail.com>
2026-01-28 17:36:41 -0700

    Initialize Android Studio project

---
+++ quanta/app/src/main/java/com/example/quanta/Shape.java
@@ -0,0 +1,11 @@
+package com.example.quanta;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = x;
+    }
+}
